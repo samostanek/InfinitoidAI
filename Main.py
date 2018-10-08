4e5bdad02b91d6a420991958462dd00510cdba2f
Samuel Stanek <samostanek@gmail.com>
2018-10-08 21:56:05 +0200

    Change1
    Basic objects + road + cells
    + Ivan

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 coins = 200
+road = []
+cells = []
 
 
 class Enemy:
@@ -13,3 +15,37 @@ class Regular(Enemy):
     def __init__(self):
         Enemy.__init__(self, 100, 0.5)
 
+
+class Cell:
+    def __init__(self, pos, upgrades=0):
+        self.pos = pos
+        self.upgrades = upgrades
+        self.tower = 0
+
+
+class Basic:
+    rng = 2
+    dmg = 10
+    atspd = 5
+    rtspd = 90
+    prspd = 1
+
+
+class Bullet:
+    def __init__(self, dmg, t, host):
+        self.dmg = dmg
+        self.t = t
+        self.host = host
+
+    def update(self):
+        self.t -= 1     # TODO: Set time delta based on tick time length
+        if self.t <= 0:
+            self.host.hp -= self.dmg
+            return True
+        return False
+    # Return True if Bullet needs to be deleted
+
+
+for i in range(1, 6):
+    cells.append(Cell([0.5, i+0.5]))
+    cells.append(Cell([2.5, i + 0.5]))
