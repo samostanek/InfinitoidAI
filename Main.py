63650ee6ff1d24a148292a05edcf17e03b912431
Samuel Stanek <samostanek@gmail.com>
2018-10-09 22:02:35 +0200

    Blbe chyby
    Co-Authored-By: HollowHero <hollowhero@users.noreply.github.com>

--- Main.py
+++ Main.py
@@ -1,19 +1,39 @@
+import math
+
 coins = 200
-road = []
+road_g = [[5, 3], [7, 3], [7, 5], [7, 7], [5, 7], [3, 7], [3, 5], [3, 3]]
 cells = []
 
 
 class Enemy:
-    pos = [1.5, 0.5]
+    pos = 0   # ++ in every loop
 
     def __init__(self, hp, speed):
         self.hp = hp
         self.speed = speed
 
+    def get_pos(self, road):
+        half = self.pos/2
+        whole = math.floor(half)
+        if road[whole][1] == road[whole + 1][1]:
+            if road[whole][0] < road[whole + 1][0]:
+                return [road[whole][0] + (self.pos - 2*whole),    # Ak to nepojde asi je chýba tu
+                        road[whole][1]]
+            else:
+                return [road[whole][0] - (self.pos - 2*whole),
+                        road[whole][1]]
+        else:
+            if road[whole][1] < road[whole + 1][1]:
+                return [road[whole][0],
+                        road[whole][1] + (self.pos - 2*whole)]
+            else:
+                return [road[whole][0],
+                        road[whole][1] - (self.pos - 2*whole)]
+
 
 class Regular(Enemy):
     def __init__(self):
-        Enemy.__init__(self, 100, 0.5)
+        Enemy.__init__(self, 100, 1)
 
 
 class Cell:
@@ -24,11 +44,29 @@ class Cell:
 
 
 class Basic:
-    rng = 2
+    rng = 4
     dmg = 10
     atspd = 5
     rtspd = 90
-    prspd = 1
+    prspd = 4
+
+    @staticmethod
+    def shot(enemy, cell):
+        dx = cell.pos[0] - enemy.get_pos(road_g)[0]
+        dy = cell.pos[1] - enemy.get_pos(road_g)[1]
+        if dx == 0:
+            if dy < 0:
+                return 270
+            else:
+                return 90
+        atan = math.atan(dy/dx)
+        if dx <= 0:
+            if atan < 0:
+                return 360 + atan
+            else:
+                return atan
+        else:
+            return 180 - atan
 
 
 class Bullet:
@@ -47,5 +85,14 @@ class Bullet:
 
 
 for i in range(1, 6):
-    cells.append(Cell([0.5, i+0.5]))
-    cells.append(Cell([2.5, i + 0.5]))
+    cells.append(Cell([1, 2*i+1]))
+    cells.append(Cell([5, 2*i + 2]))
+
+
+c = Cell([5, 5])
+c.tower = Basic()
+e = Regular()
+print(e.get_pos(road_g))
+for i in range(0, 15):
+    e.pos = i
+    print(c.tower.shot(e, c))
