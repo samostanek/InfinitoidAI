2c0ffe21caf7d032f24f96bb08b7c63dfeaa0a0e
Samuel Stanek <samostanek@gmail.com>
2018-10-11 19:39:25 +0200

    Finished rotate and shoot + comments
    Co-Authored-By: hollowhero <hollowhero@users.noreply.github.com>

--- Main.py
+++ Main.py
@@ -3,58 +3,38 @@ import math
 coins = 200
 road_g = [[5, 3], [7, 3], [7, 5], [7, 7], [5, 7], [3, 7], [3, 5], [3, 3], [5, 3]]
 cells = []
+tickrate: int = 100
 
 
-class Enemy:
-    pos = 0   # ++ in every loop
+class Enemy:        # Parent for enemy type classes
+    pos = 0         # ++ in every loop
 
     def __init__(self, hp, speed):
+        self.effects = []  # Clear array for effects
         self.hp = hp
         self.speed = speed
 
-    def get_pos(self, road):
+    def get_pos(self, road):    # Enemy.pos convert to Cartesian  TODO: catch error out of index for end of a road
         half = self.pos/2
         whole = math.floor(half)
-        if road[whole][1] == road[whole + 1][1]:
-            if road[whole][0] < road[whole + 1][0]:
-                return [road[whole][0] + (self.pos - 2*whole),    # Ak to nepojde asi je chyba tu
+        if road[whole][1] == road[whole + 1][1]:        # If Δ in horizontal direction
+            if road[whole][0] < road[whole + 1][0]:     # If direction of  is positive
+                return [road[whole][0] + (self.pos - 2*whole),
                         road[whole][1]]
-            else:
+            else:                                       # Direction of Δ is negative
                 return [road[whole][0] - (self.pos - 2*whole),
                         road[whole][1]]
-        else:
-            if road[whole][1] < road[whole + 1][1]:
+        else:                                           # Δ in vertical direction
+            if road[whole][1] < road[whole + 1][1]:     # If Δ > 0
                 return [road[whole][0],
                         road[whole][1] + (self.pos - 2*whole)]
-            else:
+            else:                                       # Δ < 0
                 return [road[whole][0],
                         road[whole][1] - (self.pos - 2*whole)]
 
-
-class Regular(Enemy):
-    def __init__(self):
-        Enemy.__init__(self, 100, 1)
-
-
-class Cell:
-    def __init__(self, pos, upgrades=0):
-        self.pos = pos
-        self.upgrades = upgrades
-        self.tower = 0
-
-
-class Basic:
-    rng = 4
-    dmg = 10
-    atspd = 5
-    rtspd = 90
-    rtangl = 0
-    prspd = 4
-
-    @staticmethod
-    def enemy_angle(enemy, cell):
-        dx = cell.pos[0] - enemy.get_pos(road_g)[0]
-        dy = cell.pos[1] - enemy.get_pos(road_g)[1]
+    def enemy_angle(self, cell):       # Get enemy angle from 0 relative to tower
+        dx = cell.pos[0] - self.get_pos(road_g)[0]
+        dy = cell.pos[1] - self.get_pos(road_g)[1]
         if dx == 0:
             if dy > 0:
                 return 270
@@ -69,29 +49,74 @@ class Basic:
         else:
             return 180 + atan
 
-    def rtt(self, enangl):      # TODO: Optimalizuj
-        if self.rtangl > enangl:
+    def addeff(self, effect):
+        self.effects.append(effect)
+
+    def update(self):
+        for effect in self.effects:
+            effect.update(self)
+
+
+class Regular(Enemy):
+    def __init__(self):
+        Enemy.__init__(self, 100, 1)
+
+
+class Cell:
+    def __init__(self, pos, upgrades=0):
+        self.pos = pos
+        self.upgrades = upgrades
+        self.tower = 0
+
+
+class Tower:
+    def __init__(self, child):
+        self.child = child
+
+    def rotate_and_shoot(self, angl):     # Rotate tower by given amount
+        if math.fabs(angl) < self.child.rtspd / tickrate:   # If angl is less than tick rotation speed
+            self.child.rtangl += angl
+            self.child.shoot()
+        else:
+            if angl == 0:
+                raise ValueError('Rotation Δ zero!')
+                pass
+            # Else if angl is positive, rotate by max speed to + else to -
+            self.child.rtangl += self.child.rtspd / tickrate if angl > 0 else - self.child.rtspd / tickrate
+
+
+class Basic(Tower):        # Tower in cell
+    rng = 4         # Basic tower properties
+    dmg = 10
+    atspd = 5
+    rtspd = 90
+    rtangl = 0
+    prspd = 4
+
+    def rtt(self, enangl):      # Tower-enemy rotate Δ function  TODO: Optimalizuj
+        if self.rtangl > enangl:    # If angle to enemy is higher than tower's
             if self.rtangl - enangl > 180:
                 return 360 - (self.rtangl - enangl)
             else:
                 return enangl - self.rtangl
-        elif self.rtangl == enangl:
+        elif self.rtangl == enangl:     # Else if they are the same, return 0
             return 0
-        else:
+        else:   # Else tower's angle is higher than to enemy
             if self.rtangl - enangl < -180:
                 return 360 + self.rtangl - enangl
             else:
                 return self.rtangl - enangl
 
 
-class Bullet:
+class Bullet:                   # Bullet effect for shots
     def __init__(self, dmg, t, host):
+        # Duration input in secs converted to ticks
         self.dmg = dmg
-        self.t = t
-        self.host = host
+        self.t = t * tickrate
+        self.host = host        # Enemy object, on which effect is applied
 
     def update(self):
-        self.t -= 1     # TODO: Set time delta based on tick time length
+        self.t -= 1
         if self.t <= 0:
             self.host.hp -= self.dmg
             return True
@@ -107,9 +132,10 @@ for i in range(1, 6):
 c = Cell([5, 5])
 c.tower = Basic()
 e = Regular()
+b = Bullet(5, 2, e)
 
 for i in range(0, 15):
     e.pos = i
     print(e. get_pos(road_g))
-    print(c.tower.rtt(Basic.enemy_angle(e, c)))
+    print(c.tower.rtt(e.enemy_angle(c)))
     print("")
